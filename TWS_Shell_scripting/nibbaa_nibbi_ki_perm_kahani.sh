87e8aa9ea88b9f5aceac20e8441af3da6d72ea55
Ubuntu <ubuntu@ip-172-31-11-14.ap-south-1.compute.internal>
2024-02-15 13:32:10 +0000

    TWSShellS

---
+++ TWS_Shell_scripting/nibbaa_nibbi_ki_perm_kahani.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# For single line comments
+
+echo "Nibba: Kaisi ho:)"
+
+echo "ibbi: Chatta Khayega"
+
+echo "Nibba: Jo tum khila do meri jan"
+
+echo "Nibbi: Zara date dekho aaj ki"
+
+date
+
+echo "Nibba: Mai kabse tmhari Entzar kar rha hu"
+
+uptime
+
+echo "Nibbi: File kaun kaun se hai tumhare pas"
+
+ls /home/ubuntu
+
+echo "Nibba: Han! mai apni file dekhi Ab tum apni File ki permission dekho kya meri mind padh skte ho"
+
+echo "Nibba: Lo tum bhi delh lo Permissions meri"
+
+ls -lts
+
+echo "Nibbi: Kitne hidden file hai tumhare pas"
+
+ls -a
+
+
